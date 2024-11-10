9a197e6e58db61c35fe0fe6b87af2086f4e09bce
Juan Coquet <69824312+juancoquet@users.noreply.github.com>
2024-11-10 14:36:11 +0000

    PositiveInt non integer float init without flooring raises

--- sensible_types/positive_int.py
+++ sensible_types/positive_int.py
@@ -1,8 +1,13 @@
 class PositiveInt:
-    def __init__(self, value: int | float):
+    def __init__(self, value: int | float, floor: bool = True):
         if value < 0:
             raise ValueError(
                 f"A PositiveInt cannot be created from a number < 0. Value passed: {value}."
+            )
+        if not floor and not value.is_integer():
+            raise ValueError(
+                f"Expected a whole number to be passed. Given: {value}."
+                "Use `floor=True` to perform a `math.floor` operation on input values."
             )
         self.__value = int(value)
 
