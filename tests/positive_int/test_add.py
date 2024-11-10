72fb03ea9fd58bf1ed501d16ee368420aa78722e
Juan Coquet <69824312+juancoquet@users.noreply.github.com>
2024-11-10 18:28:59 +0000

    PositiveInt iadd invalid float

--- tests/positive_int/test_add.py
+++ tests/positive_int/test_add.py
@@ -108,7 +108,15 @@ def test_iadd_int_raises_if_would_be_negative_value():
     with pytest.raises(ValueError):
         a += -2
 
-# todo: invalid float
+
+def test_iadd_float_raises_if_would_be_negative_value():
+    a = PositiveInt(1)
+    with pytest.raises(ValueError):
+        a += -2.0
+
+
+# todo: non int float
+
 
 def test_iadd_pos_int_to_int_returns_int():
     a = 1
