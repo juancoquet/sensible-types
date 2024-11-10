14f55299913d61cffab216cbc53589ee6150d8d9
Juan Coquet <69824312+juancoquet@users.noreply.github.com>
2024-11-10 14:48:51 +0000

    PositiveInt docstring

--- sensible_types/positive_int.py
+++ sensible_types/positive_int.py
@@ -1,5 +1,24 @@
 class PositiveInt:
     def __init__(self, value: int | float, floor: bool = True):
+        """
+        Create a new instance of `PostiveInt`.
+
+        By default non-integer input values will be floored, much like how `int(1.5)` behaves. E.g.
+        `PositiveInt(1.5) == 1  # True`.
+        If you do not wish for the input `value` to be floored, passing `floor = False` will raise a
+        `ValueError` if an invalid (non-integer) `value` is passed.
+
+        Parameters:
+        - `value: int | float` - The `value` to be converted into a `PositiveInt`. Must be >= 0.
+        - `floor: bool` - Whether or not to perform a floor operation on the input `value`. Defaults 
+          to `True`.
+
+        Raises:
+        - `ValueError`:
+          - If the input `value` is < 0.
+          - If `floor` is `False` and the input `value` is not a whole number. Note that whole number
+            `float` types such as `2.0` are valid.
+        """
         if value < 0:
             raise ValueError(
                 f"A PositiveInt cannot be created from a number < 0. Value passed: {value}."
