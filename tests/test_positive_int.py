5fe9baf0c410f57981ea9d59bbd8cb135931c151
Juan Coquet <69824312+juancoquet@users.noreply.github.com>
2024-11-10 14:53:57 +0000

    PositiveInt eq to PositiveInt false

--- tests/test_positive_int.py
+++ tests/test_positive_int.py
@@ -55,15 +55,21 @@ def test_eq_to_valid_float():
 
 def test_eq_to_invalid_float():
     i = PositiveInt(1)
-    assert not i == 1.5
+    assert i != 1.5
 
 
 def test_eq_to_invalid_type_not_implemented():
     i = PositiveInt(1)
-    assert not i == "1"
+    assert i != "1"
 
 
 def test_eq_to_postive_int_true():
     i = PositiveInt(1)
     j = PositiveInt(1)
     assert i == j
+
+
+def test_eq_to_positive_int_false():
+    i = PositiveInt(1)
+    j = PositiveInt(2)
+    assert i != j
